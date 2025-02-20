9fb761f8585aa0638e220cd4baca0da0b6e54df5
Nikita Dmitriev <dmitrevnik@gmail.com>
2025-02-20 15:09:15 +0300

    configure typescript redux and add mock counterSlice

--- src/pages/PageMain.tsx
+++ src/pages/PageMain.tsx
@@ -1,11 +1,25 @@
+import { increment } from "@/app/store/counterSlice";
 import CoreContainer from "@/components/main/CoreContainer";
+import { Button } from "@/components/ui/button";
+import { useAppDispatch } from "@/shared/hooks/useAppDispatch";
+import { useAppSelector } from "@/shared/hooks/useAppSelector";
+import React from "react";
+
+const CountDisplay = React.memo(() => {
+   const count = useAppSelector((state) => state.counter.value);
+   return <span>{count}</span>;
+});
 
 function PageMain() {
+   const dispatch = useAppDispatch();
+
    return (
       <>
          <div>
             <CoreContainer>
-               <h1 className="text-[3rem]">main</h1>
+               <Button onClick={() => dispatch(increment())}>increment</Button>
+               <CountDisplay />
+               <h1 className="">main</h1>
             </CoreContainer>
          </div>
       </>
