efc71855d1b6f84ec552a83f187a15e92ed3dd80
Nikita Dmitriev <dmitrevnik@gmail.com>
2025-02-21 01:28:07 +0300

    change container paddings

--- src/shared/ui/CoreContainer.tsx
+++ src/shared/ui/CoreContainer.tsx
@@ -1,7 +1,7 @@
 function CoreContainer({ children }: { children: React.ReactNode }) {
    return (
       <>
-         <div className="max-w-[1600px] mx-auto px-[1.5rem] relative z-10">
+         <div className="max-w-[1600px] mx-auto px-[0.8rem] relative z-10">
             {children}
          </div>
       </>
